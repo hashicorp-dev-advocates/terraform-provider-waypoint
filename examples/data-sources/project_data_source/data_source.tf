7868898496c33754c782dd54fd00020b24d9e90a
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2022-05-31 11:22:35 +0100

    refactor

--- examples/data-sources/project_data_source/data_source.tf
+++ examples/data-sources/project_data_source/data_source.tf
@@ -12,10 +12,18 @@ provider "waypoint" {
   token         = "HZCwuUtmrrpPuNwZEfzCLR6NunrkNukaMruVWSXKPAFRmf3ivYejV2PRnNPVCLKpRi8djai1QSNqJfFrZjPiBCY7SPsk7or1tgj64fcDavYPFDzFq3iykqEZgkcW9fJmzPgYNznhaZPjfffb9fm2AWavMsqbdwzBKL9e"
 }
 
-data "waypoint_project" "njack" {
-  project_name = "njack"
+data "waypoint_project" "tf-test" {
+  project_name = "tf-test"
 }
 
-output "njack_apps" {
-  value = data.waypoint_project.njack.applications
+output "tf_test_apps" {
+  value = data.waypoint_project.tf-test.applications
+}
+
+output "tf_test_variables" {
+  value = data.waypoint_project.tf-test.project_variables
+}
+
+output "tf_test_data_source_git" {
+  value = data.waypoint_project.tf-test.data_source_git
 }
