1a05a1918b946080560bc718bc30f8840d41ea47
DevOps Rob <robert.raybarnes@gmail.com>
2022-06-14 17:24:08 +0100

    runner profile data source and bug fixes

---
+++ examples/data-sources/runner_profile/runner_profile.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    waypoint = {
+      source  = "local/hashicorp/waypoint"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "waypoint" {
+  waypoint_addr = "localhost:9701"
+  token         = "rHhYzVXQBcK6NWBuN2DAAHsofoLBjPDJXKFwnZKxU9Xh13ShJs31muEV5YyJ97JhPyLsYQok1vYhpYqhWQisKuGf8DxuBRM9qbfXbap789YsBYGKFmkXvpdaLSM83moC139XkJ1rV5PR9Nwyxk64gHGk9CMroYvgG"
+}
+
+data "waypoint_runner_profile" "test" {
+  id = "01G5H84QTN0HZK9MJ7RQ8D349C"
+}
+
+output "profile_name" {
+  value = data.waypoint_runner_profile.test.profile_name
+}
+
+output "runner_default_bool" {
+  value = data.waypoint_runner_profile.test.default
+}
+
+output "oci_url" {
+  value = data.waypoint_runner_profile.test.oci_url
+}
+
+output "plugin_type" {
+  value = data.waypoint_runner_profile.test.plugin_type
+}
+
+output "environment_variables" {
+  value = data.waypoint_runner_profile.test.environment_variables
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/hashicorp-dev-advocates/terraform-provider-waypoint
 go 1.18
 
 require (
-	github.com/hashicorp-dev-advocates/waypoint-client v0.0.0-20220612142425-929b3070e504
-	github.com/hashicorp/terraform-plugin-docs v0.10.0
+	github.com/hashicorp-dev-advocates/waypoint-client v0.0.0-20220614092443-25f9a4b17102
+	github.com/hashicorp/terraform-plugin-docs v0.10.1
 	github.com/hashicorp/terraform-plugin-log v0.4.1
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.17.0
 )
